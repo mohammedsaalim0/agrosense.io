1945fb14afe4d049721f1c1a16007f75a4542ac5
mohammed-saalim <mohammedsaalimismil@gmail.com>
2026-04-27 23:30:41 +0530

    FIX: Inline buildCommand, stable Django version, and basic static storage

--- agrosense_project/settings.py
+++ agrosense_project/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 # PWA Settings
 PWA_MANIFEST_PATH = 'static/manifest.json'
